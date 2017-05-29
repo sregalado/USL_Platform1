6ae9f97c386b0cec4d7239fbef30e5cca8c1a107
sregalado <sregalado@s.paulsmiths.edu>
2017-05-29 09:16:43 -0400

    Commit

--- dataUpload.bat
+++ dataUpload.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\R\R-3.3.0\bin\i386\Rcmd.exe" BATCH "C:\Users\research\My Documents\buoy\buoyPlots.R"
+"C:\Users\sregalado\Documents\R\R-3.3.2\bin\x64\Rcmd.exe" BATCH "F:\USL_EMP\USL_Platform\buoyPlots.R"
